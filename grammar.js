88feaa4d6d0ec08f7dbf54e4ce1e1a986bc05618
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2021-01-25 17:12:50 +0100

    Fix literals and other small things

--- grammar.js
+++ grammar.js
@@ -2,29 +2,46 @@
 //
 // The SCIDE lexer file:
 // https://github.com/supercollider/supercollider/blob/608bb981162c2c26f0a32c09d82557b29774a32e/editors/sc-ide/core/sc_lexer.cpp
+//
+//
+// TODO:
+// - Unary
+// - Binary
+// - Block comments
 
+function sepBy1(sep, rule) {
+  return seq(rule, repeat(seq(sep, rule)))
+}
+
+function sepBy(sep, rule) {
+  return optional(sepBy1(sep, rule))
+}
 module.exports = grammar({
 	name: 'supercollider',
 
-	 // The name of a token that will match keywords for the purpose of the keyword extraction optimization.
+	extras: $ => [/\s/, $.line_comment],
+
+	// The name of a token that will match keywords for the purpose of the keyword extraction optimization.
 	word: $ => $.identifier,
 	conflicts: $ => [
 		[$.unnamed_argument, $.named_argument]
 	],
 
-
 	rules: {
 
 		source_file: $ => repeat($._expression),
 
-		_expression: $ => seq($._expression_statement, ";"),
+		_expression: $ => choice(
+			$.code_block,
+			seq($._expression_statement, ";"),
+		),
 
 		_expression_statement: $ => choice(
 			$.function_block,
 			$.function_definition,
 			$.function_call,
 			$.variable_definition,
-			$.return_statement
+			// $.return_statement
 		),
 
 		_value: $ => choice(
@@ -46,11 +63,19 @@ module.exports = grammar({
 		function_call: $ => seq(
 			$.variable, 
 			".", 
-			optional("value"), 
+			optional($.identifier), 
 			"(", 
 			$.parameter_call_list, 
 			")"
 		),
+		
+		function_block: $ => seq(
+			'{',
+			optional($.parameter_list),
+			repeat($._expression),
+			'}'
+		),
+
 
 		// Definition of parameters in function
 		parameter_list: $ => choice(
@@ -58,9 +83,13 @@ module.exports = grammar({
 			seq('|', sepBy(',', $.argument), '|')
 		),
 
+		// For definition lists
+		argument: $ => seq($.identifier, optional(seq("=", $.literals))),
+
 		// When supplying arguments to a function call
-		parameter_call_list: $ => seq($.argument, optional(sepBy(',', $.argument))),
-		argument: $ => choice(
+		parameter_call_list: $ => sepBy1(',', $.argument_calls),
+
+		argument_calls: $ => choice(
 			$.named_argument,
 			$.unnamed_argument,
 		),
@@ -85,8 +114,14 @@ module.exports = grammar({
 		literals: $ => choice(
 			$.number,
 			$.symbol,
+			$.char,
+			$.string
 		),
-		number: $ => choice($.integer, $.float),
+		number: $ => choice(
+			$.integer, 
+			$.float,
+			$.hexinteger
+		),
 		integer: $=>/\d+/,
 		hexinteger: $=> /0x(\\d|[a-f]|[A-F])+/,
 		float: $=> seq($.integer, ".", $.integer),
@@ -95,35 +130,21 @@ module.exports = grammar({
 			seq("'", $.identifier, "'"),
 		),
 		char: $ => /\$./,
+		string: $ => seq("\"", optional($.identifier), "\""),
 
 		//////////////
 		//  Blocks  //
 		//////////////
-		
-		block: $ => choice(
-			$._code_block,
-			// $._function_block
-		),
-
-		function_block: $ => seq(
-			'{',
-			optional($.parameter_list),
-			repeat($._expression),
-			'}'
-		),
-
-		_code_block: $ => seq(
+		code_block: $ => seq(
 			'(',
 			repeat($._expression),
 			')'
 		),
 
+		/////////////////
+		//  Variables  //
+		/////////////////
 		
-		_definition: $=> seq(
-			$.variable,
-			'='
-		),
-
 		variable: $ => choice(
 			$.environment_var,
 			$.local_var
@@ -137,17 +158,20 @@ module.exports = grammar({
 		identifier: $ => /[a-zA-Zα-ωΑ-Ωµ_][a-zA-Zα-ωΑ-Ωµ\d_]*/,
 
 		return_statement: $ => seq("^", choice($.variable, $.literals)),
+
+		////////////////
+		//  Comments  //
+		////////////////
+
+		comment: $ => choice(
+			$.line_comment,
+			// $.block_comment
+		),
+
+		line_comment: $ => token(seq( '//', /.*/)),
+
+		// block_comment: $ => seq(),
+
 	}
 });
 
-function commaSeq(rule) {
-	return seq(rule, repeat(seq(',', rule)))
-}
-
-function sepBy1(sep, rule) {
-  return seq(rule, repeat(seq(sep, rule)))
-}
-
-function sepBy(sep, rule) {
-  return optional(sepBy1(sep, rule))
-}
